552cbaa6e7426798f56e99faebd679e29473e10e
TerryMin <15896938825@163.com>
2022-06-13 21:40:01 +0800

    git test

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@
  * @Author: TerryMin
  * @Date: 2021-12-28 11:34:44
  * @LastEditors: TerryMin
- * @LastEditTime: 2022-06-11 14:44:21
+ * @LastEditTime: 2022-06-13 21:39:18
  * @Description: file not
  */
 type Value = { name: "zero2one"; age: 12; sex: "boy" }["name" | "age"] // 等价于 type Value = "zero2one"
 
-const nameList: Value = 12
+const nameList: Value = 'zero2one'
