695f5313c080670e3167f24c1f7097a08476d939
huangminguang <17621481287@163.com>
2022-12-28 11:34:48 +0800

    js 传值与传址

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
  * @Author: TerryMin
  * @Date: 2022-06-11 14:41:52
  * @LastEditors: TerryMin
- * @LastEditTime: 2022-06-15 10:50:45
+ * @LastEditTime: 2022-12-06 18:56:13
  * @Description: file not
  */
-// const reg = /^a{1,3}$/g;
-// const result = ("ahooka").match(/^a{1,3}$/g);
-console.log(("ahook,ttwe era au").match(/^a[\w]*$/gm));
-// console.log(result);
+class Animals {
+  static instance=11;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log(Animals);
+  }
+}
+
+let cat=new Animals();
