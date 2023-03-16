ecfbab8e6b5a4c8a7c04ea595a05690570c61bad
huangminguang <17621481287@163.com>
2023-03-16 14:26:29 +0800

    feat: 正则表达式

--- css-learning/animation/README_one.md
+++ css-learning/animation/README_one.md
@@ -2,7 +2,7 @@
  * @Author: TerryMin
  * @Date: 2023-02-02 18:53:35
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-02-05 15:06:42
+ * @LastEditTime: 2023-02-17 15:15:43
  * @Description: file not
 -->
 
@@ -10,7 +10,7 @@
 
 一 [setTimeOut 与 requestAnimationFrame 区别](https://blog.csdn.net/weixin_40851188/article/details/89669416)
 
-1. 引擎层面：setTimeout 属于 JS 引擎，存在事件轮询，存在事件队列。requestAnimationFrame 属于 GUI 引擎，发生在渲 染过程的中重绘重排部分，与电脑分辨路保持一致。
+1. 引擎层面：setTimeout 属于 JS 引擎，存在事件轮询，存在事件队列。requestAnimationFrame 属于 GUI 引擎，发生在渲 染过程的中重绘重排部分，与电脑分辨率保持一致。
 
 2. 性能层面：当页面被隐藏或最小化时，定时器 setTimeout 仍在后台执行动画任 务。当页面处于未激活的状态下，该页面的屏幕刷新任 务会被系统暂停，requestAnimationFrame 也会停止。
 
--- js-basic-learning/regular-expression/index.js
+++ js-basic-learning/regular-expression/index.js
@@ -2,56 +2,13 @@
  * @Author: TerryMin
  * @Date: 2022-06-15 11:23:58
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-01-30 09:52:20
+ * @LastEditTime: 2023-03-16 14:23:19
  * @Description: file not
  */
-function is_weixn() {
-	var ua = navigator.userAgent.toLowerCase()
-	//  == 会进行类型转换 即 ['aa']=='aa' = > true
-	if (ua.match(/MicroMessenger/i) == "micromessenger") {
-		return true
-	} else {
-		return false
-	}
-}
+const str='Hello';
+const result=str.replace(/(?!l)/g,'#');
+// console.log(result);
 
-const matchStr = "ahook,ttwe era au".match(/a[\w]*/g)
-const matchStr1 = "ahook,ttwe era au".match(/^a[\w]*$/g)
-// console.log(matchStr,matchStr1);
-
-const str = "aa12bb0cc789"
-const newStr = str.replace(/\d/g, function (matchStr, index, sourceStr) {
-	console.log(matchStr, index, sourceStr);
-	const result = parseInt(matchStr) + 1
-	return result
-})
-console.log(55,newStr);
-const string = "123 1234 12345 123456";
-// console.log(string.match(/[\d]{2,5}?/g));
-
-// (?=p)，其中p是一个子模式，即p前面的位置。
-const string1="hello".replace(/(?=l)/g,'#');
-// console.log(string1);
-
-// (?!p)就是(?=p)的反面意思
-const string2="hello".replace(/(?!l)/g,'#');
-// console.log(string2);
-
-// 金额逗号分割
-const string3='123456789'.replace(/\B(?=(\d{3})+\b)/g,',');
-// console.log(string3);
-
-const string4=(/(?=.*[0-9])^[0-9A-Za-z]{2,12}$/).test('eee');
-// console.log(string4);
-
-const regex = /^((\d)(\d(\d)))\1\2\3\4$/;
-const string5 = "1231231233";
-// console.log( regex.test(string5) ); // true
-// console.log( RegExp.$1 ); // 123
-// console.log( RegExp.$2 ); // 1
-// console.log( RegExp.$3 ); // 23
-// console.log( RegExp.$4 ); // 3
-
-
-
-
+const numStr='123456789';
+const result1=numStr.replace(/(?=(\d{3})+$)/g,",");
+console.log(result1);
